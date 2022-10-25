c3f2c0b3a5c85aaaacf5fbd2462c04c2c85ef6cf
HipolitoDeveloper <hipolitodeveloper@gmail.com>
2022-10-25 11:33:02 -0300

    end of the structure

--- bowling.js
+++ bowling.js
@@ -4,45 +4,57 @@ const strike = "x";
 const input = "165/251/X2/71XX1/7";
 
 function calculateScore(input) {
-  const throwScore = input.split("");
-  let gameScore = 0;
-  throwScore.forEach((score, index) => {
-    const hasNextRound = index + 1 !== throwScore.length || index + 2 !== throwScore.length
+    const throwScore = input.split("");
+    let gameScore = 0;
+    throwScore.forEach((score, index) => {
+        const hasNextRound = index + 1 <= throwScore.length && index + 2 <= throwScore.length
 
-    const sc = handleScoreType(score, index, throwScore, hasNextRound);
-    console.log(throwScore[index + 1])
+        const sc = handleScoreType(score, index, throwScore, hasNextRound);
+        console.log(throwScore[index + 1])
 
-    gameScore += sc;
-  });
+        gameScore += sc;
+    });
 
-  return gameScore;
+    return gameScore;
 }
 
 function handleScoreType(score, index, throwScore, hasNextRound) {
 
-  switch (score) {
-    case "/":
-      const nextRoundScore = hasNextRound && handleScoreWithoutBonus(throwScore[index + 1], throwScore, index) === 0 ?handleScoreType(throwScore[index + 1], index + 1, throwScore, hasNextRound) : 0
-      return (10 - Number(throwScore[index - 1])) + nextRoundScore
-    case "X":
-      const nextRoundScor = hasNextRound && handleScoreWithoutBonus(throwScore[index + 1], throwScore, index) === 0 ? handleScoreType(throwScore[index + 1], index + 1, throwScore, hasNextRound) : 0
-      const nextSecondRoundScore = hasNextRound && handleScoreWithoutBonus(throwScore[index + 2], throwScore, index +1) === 0 ? handleScoreType(throwScore[index + 2], index + 2, throwScore, hasNextRound)  : 0
+    switch (score) {
+        case "/":
+            let nextRoundScore = hasNextRound && handleScoreWithoutBonus(throwScore[index + 1], throwScore, index)
 
-      return 10 + nextRoundScor  + nextSecondRoundScore;
-    default:
-      return Number(score);
-  }
+            if (nextRoundScore === 0) {
+                nextRoundScore = handleScoreType(throwScore[index + 1], index + 1, throwScore, hasNextRound)
+            }
+            return (10 - Number(throwScore[index - 1])) + nextRoundScore
+        case "X":
+            let nextRoundScor = hasNextRound && handleScoreWithoutBonus(throwScore[index + 1], throwScore, index)
+            let nextSecondRoundScore = hasNextRound && handleScoreWithoutBonus(throwScore[index + 2], throwScore, index + 1)
+
+            if (nextRoundScor === 0) {
+                nextRoundScor = handleScoreType(throwScore[index + 1], index + 1, throwScore, hasNextRound)
+            }
+
+            if (nextSecondRoundScore === 0) {
+                nextSecondRoundScore = handleScoreType(throwScore[index + 2], index + 2, throwScore, hasNextRound)
+            }
+
+            return 10 + nextRoundScor + nextSecondRoundScore;
+        default:
+            return hasNextRound ? Number(score) : 0;
+    }
 }
 
 function handleScoreWithoutBonus(score, throwScore, index) {
-  switch (score) {
-    case "/":
-      return (10 - Number(throwScore[index]))
-    case "X":
-      return 10
-    default:
-      return 0
-  }
+    switch (score) {
+        case "/":
+            return (10 - Number(throwScore[index]))
+        case "X":
+            return 10
+        default:
+            return 0
+    }
 }
 
 
